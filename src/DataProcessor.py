13b6ba058d1dad78c0b0e7184011d0fb18117b25
Julia Klos <julia.klos.stud@pw.edu.pl>
2021-05-06 17:36:04 +0200

    starter classes

---
+++ src/DataProcessor.py
@@ -0,0 +1,5 @@
+
+class DataProcessor():
+
+    def load(path):
+        
---
+++ src/GSP.py
@@ -0,0 +1,6 @@
+from SequentialPatternAlgorithm import SequentialPatternAlgorithm
+
+class GSP(SequentialPatternAlgorithm):
+
+    def run(self):
+        return self._data
