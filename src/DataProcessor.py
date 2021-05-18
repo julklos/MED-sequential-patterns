455b50004654eff29ccfe5e26267e1fdaa43c90a
Patrycja <patrycja.cieplicka@gmail.com>
2021-05-18 18:36:23 +0200

    read txt and spmf file in one function

--- src/DataProcessor.py
+++ src/DataProcessor.py
@@ -1,30 +1,29 @@
 from Sequence import Sequence
 class DataProcessor():
 
-    def load(self,path, spmf = True):
-        if spmf:
-            return self.load_spmf(path)
-        else:
-            return self.load_text(path)
+    def __init__(self, newline = "-2", splitter = "-1"):
+        self.newline = newline
+        self.splitter = splitter
 
-    def load_spmf(self, path):
+    def load(self,path):
+        return self.load_file(path)
+
+    def load_file(self, path):
         file = open(path,mode='r')
         # read all lines at once
         content = file.read()
         # close the file
         file.close()
-        content = content.replace("\n", "")
-        sequences = content.split("-2")
+        if self.newline != "\n" :
+            content = content.replace("\n", "")
+        sequences = content.split(self.newline)
         # delete empty
         sequences = [x for x in sequences if x]
         # create array
-        sequences_obj = [ Sequence(idx, self.preprocess_spmf_itemset(val) ) for idx, val in enumerate(sequences) ]
+        sequences_obj = [ Sequence(idx, self.preprocess_spmf_itemset(val, self.splitter) ) for idx, val in enumerate(sequences) ]
         return sequences_obj
 
-    def loaf_text(self, path):
-        return
-
-    def preprocess_spmf_itemset(self, itemset, splitter = "-1"):
+    def preprocess_spmf_itemset(self, itemset, splitter):
         # split itemsets
         itemset = itemset.split(splitter)
         # delete whitespaces
--- src/main.py
+++ src/main.py
@@ -2,10 +2,15 @@ from PrefixSpan import PrefixSpan
 from DataProcessor import DataProcessor
 
 if __name__ == "__main__":
-    dp = DataProcessor()
-    data = dp.load("../data/short_d.spmf", True)
+    # TODO: pętla - wpisywanie nazwy pliku,  sprawdzenie czy spfm i txt
+    dp = DataProcessor(newline="\n", splitter=" ")
+    try :
+        data = dp.load("data/simple.txt")
+    except OSError:
+        print("Nie można otworzyć pliku")
+
     for seq in data:
         print(seq)
 
-    # al1 = PrefixSpan("costam")
-    # print( al1.run() )
+    al1 = PrefixSpan(data)
+    print( al1.run() )
