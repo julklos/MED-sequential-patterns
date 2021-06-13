b846b042bbc164fc7f7397f4468a83bad43575ec
Julia Klos <julia.klos.stud@pw.edu.pl>
2021-06-13 15:39:24 +0200

    windows

--- src/Analyze.py
+++ src/Analyze.py
@@ -31,7 +31,7 @@ def create_config(confname, algorithm, limit, input, output, min_support, max_le
 def run_spmf(algorithm, input_path, output_path, min_support, max_length):
 
     spmf = Spmf(algorithm, input_filename=input_path, output_filename=output_path, 
-                        spmf_bin_location_dir="./env/lib/python3.8/site-packages/spmf/", arguments=[min_support, max_length])
+                        spmf_bin_location_dir="../env/Lib/site-packages/spmf/", arguments=[min_support, max_length])
     
     results =  spmf.run()
     return results
@@ -90,12 +90,12 @@ def compare_output_files(seqalg_out, spmf_out):
 if __name__ == "__main__":
 
     #TO DO: MOZNA STWORZYC PETLE DLA JAKICH PARAMETROW
-    rspmf = result_spmf( run_spmf("PrefixSpan",'data/short_d.spmf', "output.spmf",2, 200) )
-    ralg = result_seqalgorithms( run_seqalgorithms("PrefixSpan", 'data/short_d.spmf', "output_short.json", 2, 4, None, 0,"config.conf" ,-1))
+    rspmf = result_spmf( run_spmf("PrefixSpan",'../data/short_d.spmf', "output.spmf",2, 200) )
+    ralg = result_seqalgorithms( run_seqalgorithms("PrefixSpan", 'data/short_d.spmf', "output_short.json", 2, 4, None, 0,"config.cfg" ,-1))
 
     results_file = 'test.csv'
     with open(results_file, 'a') as f:
-        f.write('algorithm, program,imput_file, output_file, min_support, max_length, min_length, limit, time, freq_seq\n')
+        f.write('algorithm, program,input_file, output_file, min_support, max_length, min_length, limit, time, freq_seq\n')
 
     with open(results_file, 'a') as f:
         f.write('PrefixSpan, seqalgorithms ,data/short_d.spmf, output_short.json, 2, 4, 0, None, {},{}\n'.format(ralg['time'], ralg['found_seq']))
